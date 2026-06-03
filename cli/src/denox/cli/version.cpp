ed2ca449a279279941ace936c3b3d43ea35dfefa
kistenklaus <karlsasssie@gmail.com>
2026-06-03 14:24:18 +0200

    switching LICENSE to GPL3

--- cli/src/denox/cli/version.cpp
+++ cli/src/denox/cli/version.cpp
@@ -1,5 +1,16 @@
 #include "version.hpp"
+#include "denox/common/commit_hash.hpp"
 #include "denox/common/version.hpp"
 #include <iostream>
 
-void version() { std::cerr << "denox " << denox::version() << std::endl; }
+void version() {
+  std::cout << "denox " << denox::version()
+            << " (https://github.com/kistenklaus/denox.git at "
+            << denox::commit_hash() << ")" << std::endl;
+  std::cout << "Copyright (C) 2025-2026 Karl Sassie" << std::endl;
+  std::cout << "This is free software; see the source for copying conditions."
+            << std::endl;
+  std::cout << "There is NO warranty; not even for MERCHANTABILITY or FITNESS "
+               "FOR A PARTICULAR PURPOSE.\n"
+            << std::endl;
+}
